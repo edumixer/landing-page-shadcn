5a0424b00638ad0a5f636c9137f66a260492d62a
edumixer <edu.cena.wf@gmail.com>
2024-12-18 16:55:24 -0300

    feat(mock): add file containing mock datas

---
+++ src/mock/features.ts
@@ -0,0 +1,62 @@
+interface FeatureProps {
+  description: string;
+  image: string;
+}
+
+export const features: FeatureProps[] = [
+  // first page
+  {
+    description: "Organize your daily job enhance your life performance",
+    image:
+      "https://s3-alpha-sig.figma.com/img/df70/51d7/36bf5a83a66136ade04460e824cdc7c2?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n3GG0vRyaE9En2wVEr5tN-vulbyrgpdWcQtq4HOVqTLd4-dBhG1N-ShSfwrX-IjbMeng~vIH~LE5SAby8zUQaAgqKclaTS-KKf-pxXdT6oIPN7fCMIn5rMSV9ypCEKsX73EXMehVbeF7~ungn26Q~KpbQBj2plxL8-rUTCmqU3SYNF6HEBGEzCv2eroTtz-XG4cNI9a2pyISJduE0iI0emjvs-AW5h914I9qX-dTjFFZlKtYxsrx-tEFz3614WWv78a~g43yk9Y~iSSEvIaV0m0UR6OmSGpa~3LvyfyiQL6qs6c00Izfo02m3K-HVgtSEUBWP4EeWVBT-PZql6916w__",
+  },
+  {
+    description:
+      "Mark one activity as done makes your brain understand the power of doing",
+    image:
+      "https://s3-alpha-sig.figma.com/img/c525/7edf/ab9e8a94f824eb12ba3113e122597353?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZqUAGEerl~NElDBBVCsFGSWwD8CmaixN7LwkwwKJZsfH7cTuG-hGRhMRpq7Vj3jMwNGpVA4PWVHixlYMVFraJqNR3xOEOxDsvx3WpYBsEPu~u-NQpsOZVoEne9CBxoc0p2b7kQeiUkCKGKUya5bJta8QXJ8dmQ63-AXMtXbUBByTOMOmoD85BluMg6gKF4P5jqWXfB1pMkD-~LzvJ-r0fWv9t5PdM-ZYesa~~q4i95wg4V2Xn~KSl728KxAoojyju6955Sfhmyx4ENjbyqBM1YuHbPSvyo3dZXcBDlDfyrap7H6RwmYMZIUOwvmMpQ1aHFZbhxk3nDAbHw2BiuatuA__",
+  },
+  {
+    description:
+      "Careful with misunderstanding the difference between a list of things and a list of desires.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/1063/8c16/66e94511001413355468fc721c26e5c7?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cPrKjHKbu~Ikj54W8g365q4kXUfuAwmg1ATa2972Lxrzo-6DQdfOaus3v3K1QHmZUhutOOfwqUF9U77f6ogpGT8I0Rd0cpd9SXo4zhyUch~8o5pN3wdAICnwTbhkgcxKdEr~l5AONbeHNWWGx0m8flTMgjKybcn72aoGnZxYuwfLHebqeCl5W~ch~H-E3A40f3MMyk5SOHk~Lq48qUTt7k5dfxqSh6T2ZiJ~IF7vtoZzYduTsfKnzp~OrGFgWY9hrcfy~cZjLxcPSVMljjvKOCaK-Sb7R3xOaszcygGeaZib-76HA-NCzu2aBh~gSlMO0AE7DXKSywGBQjkEd2lkuw__",
+  },
+
+  // second page
+  {
+    description:
+      "Leader teams are made up of people who share the same vision and goals.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9mCijoxw_NtqMcEJqAgMaWTcR2udLXVSvcA&s",
+  },
+  {
+    description: "Cooperation is the key to success in any project.",
+    image:
+      "https://blog.kyoceradocumentsolutions.com.au/hubfs/why-collaboration-drives-profit-1.jpg",
+  },
+  {
+    description:
+      "The team is made up of people who share the same vision and goals.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpqNpR1lAky-WG3Fnn3LM0_fPyHmCZEluqu8CEXUOUFmMN2nh4lOsmjwp_8xsBKGpOs2s&usqp=CAU",
+  },
+
+  // third page
+  {
+    description:
+      "Working with a team of people who share the same vision and goals is a great way to succeed in any project.",
+    image:
+      "https://training-hukumsdm.com/wp-content/uploads/2018/08/hukumsdm10.jpg",
+  },
+  {
+    description:
+      "Greatly increase the value of your business with our services.",
+    image:
+      "https://previews.123rf.com/images/wavebreakmediamicro/wavebreakmediamicro1306/wavebreakmediamicro130608519/20586367-serious-business-people-looking-at-camera-during-a-meeting-in-the-meeting-room.jpg",
+  },
+  {
+    description: "Colaboration is the key to success in any project.",
+    image: "https://i.imgur.com/bf7hn51.jpg",
+  },
+];
