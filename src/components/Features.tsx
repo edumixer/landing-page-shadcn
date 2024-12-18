a79ccb2776b81d6bc940a670ad46b746ef23e89f
edumixer <edu.cena.wf@gmail.com>
2024-12-18 16:59:08 -0300

    feat(Carousel): add pagination in carousel to display 3 itens per page and adjust layout

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,52 +6,34 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { features } from "@/mock/features";
+import { useState } from "react";
 
-interface FeatureProps {
-  description: string;
-  image: string;
-}
-
-const features: FeatureProps[] = [
-  {
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image:
-      "https://s3-alpha-sig.figma.com/img/df70/51d7/36bf5a83a66136ade04460e824cdc7c2?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n3GG0vRyaE9En2wVEr5tN-vulbyrgpdWcQtq4HOVqTLd4-dBhG1N-ShSfwrX-IjbMeng~vIH~LE5SAby8zUQaAgqKclaTS-KKf-pxXdT6oIPN7fCMIn5rMSV9ypCEKsX73EXMehVbeF7~ungn26Q~KpbQBj2plxL8-rUTCmqU3SYNF6HEBGEzCv2eroTtz-XG4cNI9a2pyISJduE0iI0emjvs-AW5h914I9qX-dTjFFZlKtYxsrx-tEFz3614WWv78a~g43yk9Y~iSSEvIaV0m0UR6OmSGpa~3LvyfyiQL6qs6c00Izfo02m3K-HVgtSEUBWP4EeWVBT-PZql6916w__",
-  },
-  {
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image:
-      "https://s3-alpha-sig.figma.com/img/c525/7edf/ab9e8a94f824eb12ba3113e122597353?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZqUAGEerl~NElDBBVCsFGSWwD8CmaixN7LwkwwKJZsfH7cTuG-hGRhMRpq7Vj3jMwNGpVA4PWVHixlYMVFraJqNR3xOEOxDsvx3WpYBsEPu~u-NQpsOZVoEne9CBxoc0p2b7kQeiUkCKGKUya5bJta8QXJ8dmQ63-AXMtXbUBByTOMOmoD85BluMg6gKF4P5jqWXfB1pMkD-~LzvJ-r0fWv9t5PdM-ZYesa~~q4i95wg4V2Xn~KSl728KxAoojyju6955Sfhmyx4ENjbyqBM1YuHbPSvyo3dZXcBDlDfyrap7H6RwmYMZIUOwvmMpQ1aHFZbhxk3nDAbHw2BiuatuA__",
-  },
-  {
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image:
-      "https://s3-alpha-sig.figma.com/img/1063/8c16/66e94511001413355468fc721c26e5c7?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cPrKjHKbu~Ikj54W8g365q4kXUfuAwmg1ATa2972Lxrzo-6DQdfOaus3v3K1QHmZUhutOOfwqUF9U77f6ogpGT8I0Rd0cpd9SXo4zhyUch~8o5pN3wdAICnwTbhkgcxKdEr~l5AONbeHNWWGx0m8flTMgjKybcn72aoGnZxYuwfLHebqeCl5W~ch~H-E3A40f3MMyk5SOHk~Lq48qUTt7k5dfxqSh6T2ZiJ~IF7vtoZzYduTsfKnzp~OrGFgWY9hrcfy~cZjLxcPSVMljjvKOCaK-Sb7R3xOaszcygGeaZib-76HA-NCzu2aBh~gSlMO0AE7DXKSywGBQjkEd2lkuw__",
-  },
-  {
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
-    image:
-      "https://s3-alpha-sig.figma.com/img/1063/8c16/66e94511001413355468fc721c26e5c7?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cPrKjHKbu~Ikj54W8g365q4kXUfuAwmg1ATa2972Lxrzo-6DQdfOaus3v3K1QHmZUhutOOfwqUF9U77f6ogpGT8I0Rd0cpd9SXo4zhyUch~8o5pN3wdAICnwTbhkgcxKdEr~l5AONbeHNWWGx0m8flTMgjKybcn72aoGnZxYuwfLHebqeCl5W~ch~H-E3A40f3MMyk5SOHk~Lq48qUTt7k5dfxqSh6T2ZiJ~IF7vtoZzYduTsfKnzp~OrGFgWY9hrcfy~cZjLxcPSVMljjvKOCaK-Sb7R3xOaszcygGeaZib-76HA-NCzu2aBh~gSlMO0AE7DXKSywGBQjkEd2lkuw__",
-  },
-];
+const featuresPerPage = 3;
 
 export const Features = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const totalPages = Math.ceil(features.length / featuresPerPage);
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
+  };
+
   return (
-    <section id="features" className="container py-24 sm:py-32 space-y-8">
-      <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-        Organize your life{" "}
-        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          achieve good things
-        </span>
-      </h2>
+    <section id="features" className="container py-24 sm:py-32">
+      <div className="relative">
+        <div className="absolute bg-green-500 w-full h-[520px] rounded-lg -z-10"></div>
+        <h2 className="text-5xl font-bold text-white z-10 pl-12 pt-16">
+          good things
+        </h2>
+      </div>
 
-      <div className="relative pt-10">
-        <div className="bg-green-500 w-[90%] h-[90%] rounded-lg absolute transform -translate-x-6 -translate-y-8"></div>
-
+      <div className="relative translate-x-14 translate-y-6 pt-10 bottom-5">
         <Carousel
           opts={{
             align: "start",
@@ -59,30 +41,33 @@ export const Features = () => {
           }}
           className="relative w-full"
         >
-          <CarouselContent className="-ml-2 md:-ml-4">
+          <CarouselContent
+            className="relative transition-transform duration-300"
+            style={{
+              transform: `translateX(-${currentPage * 100}%)`,
+            }}
+          >
             {features.map(({ description, image }, index) => (
               <CarouselItem
                 key={index}
                 className="pl-2 md:pl-4 md:basis-1/2 lg:basis-1/3"
               >
-                <Card className="h-full">
-                  <div className="absolute right-0 w-8 h-16" />
-
+                <Card className="h-full shadow-lg rounded-lg">
                   <img
                     src={image}
                     alt={description}
                     className="w-full h-48 object-cover"
                   />
-
                   <CardContent className="pt-6 space-y-4">
-                    <span className="inline-block text-sm px-4 py-1 rounded-full border border-gray-200">
+                    <span className="inline-block text-sm px-4 py-1 rounded-full border border-gray-200 text-gray-600">
                       function
                     </span>
-                    <p className="font-regular text-lg">{description}</p>
+                    <p className="font-regular text-lg text-gray-800">
+                      {description}
+                    </p>
                   </CardContent>
-
-                  <CardFooter className="flex-col items-start gap-4">
-                    <button className="text-primary hover:underline">
+                  <CardFooter>
+                    <button className="text-green-500 hover:underline">
                       read more
                     </button>
                   </CardFooter>
@@ -90,9 +75,25 @@ export const Features = () => {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="hidden md:flex" />
-          <CarouselNext className="hidden md:flex" />
+
+          <CarouselPrevious
+            className="hidden md:flex"
+            onClick={handlePrevious}
+          />
+          <CarouselNext className="hidden md:flex" onClick={handleNext} />
         </Carousel>
+      </div>
+
+      {/* Indicadores de navegação */}
+      <div className="flex justify-center mt-10">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <div
+            key={index}
+            className={`w-3 h-3 mx-2 rounded-full ${
+              currentPage === index ? "bg-green-500" : "bg-gray-300"
+            }`}
+          ></div>
+        ))}
       </div>
     </section>
   );
